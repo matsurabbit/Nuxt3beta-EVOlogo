25633aece91a39b174abd5c3dee6e43ef6928cb8
matsurabbit <matsu.rabbit@gmail.com>
2022-03-31 20:31:29 +0900

    2 files modified, 2 added

--- app.vue
+++ app.vue
@@ -1,17 +1,23 @@
 <script setup lang="ts">
-  import EVOlogo1 from "EVOlogo1.vue"
+  import EVOlogo1 from "./components/EVOlogo1.vue"
+  import EVOlogo2 from "./components/EVOlogo2.vue"
+  import EVOlogo3 from "./components/EVOlogo3.vue"
 </script>
 <template>
   <div class="container">
+    <EVOlogo2/>
+    <EVOlogo3/>
     <EVOlogo1/>
   </div>
 </template>
 
 <style scoped>
 .container {
-  width: 100%;
-  height: 100%;
+  /*width: 100%;*/
   display: grid;
-  place-items: center;
+  grid-template-rows: repeat(3, 200px);
+  grid-auto-columns: 1fr;
+  justify-content: start;
+  gap: 40px;
 }
 </style>
